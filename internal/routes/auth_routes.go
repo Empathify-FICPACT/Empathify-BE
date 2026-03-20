ed7a04e28ca1dc309b5ad9e54ab9cf07ae724fe2
GantangSatria <gantang610@gmail.com>
2026-03-20 15:28:23 +0700

    feat(auth): added auth routes

---
+++ internal/routes/auth_routes.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v3"
+
+	"github.com/Empathify-FICPACT/Empathify-BE/internal/handler"
+)
+
+func RegisterAuthRoutes(router fiber.Router, authHandler *handler.AuthHandler) {
+	auth := router.Group("/auth")
+
+	// email
+	auth.Post("/register", authHandler.RegisterEmail)
+	auth.Post("/login", authHandler.LoginEmail)
+
+	// google oauth
+	auth.Get("/google", authHandler.GoogleLogin)
+	auth.Get("/google/callback", authHandler.GoogleCallback)
+}
--- internal/routes/route.go
+++ internal/routes/route.go
@@ -3,12 +3,24 @@ package routes
 import (
 	"github.com/gofiber/fiber/v3"
 	"github.com/jackc/pgx/v5/pgxpool"
+
+	"github.com/Empathify-FICPACT/Empathify-BE/internal/handler"
+	"github.com/Empathify-FICPACT/Empathify-BE/internal/provider"
+	"github.com/Empathify-FICPACT/Empathify-BE/internal/repository"
+	"github.com/Empathify-FICPACT/Empathify-BE/internal/service"
 )
 
 func Route(app *fiber.App, db *pgxpool.Pool) {
+	authRepo := repository.NewAuthRepository(db)
+	authService := service.NewAuthService(authRepo)
+	googleOAuth := provider.NewGoogleOAuthProvider()
+	authHandler := handler.NewAuthHandler(authService, googleOAuth)
+
 	api := app.Group("/api/v1")
 
 	api.Get("/", func (c fiber.Ctx) error {
 		return c.JSON(fiber.Map{"status": "ok"})
 	})
+
+	RegisterAuthRoutes(api, authHandler)
 }
