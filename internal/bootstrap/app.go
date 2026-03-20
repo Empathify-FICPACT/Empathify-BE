a1ed8d488f3ec5b8bdd88171919a66f4781e4348
GantangSatria <gantang610@gmail.com>
2026-03-20 16:33:16 +0700

    feat(cors): added cors rule for testing

--- internal/bootstrap/app.go
+++ internal/bootstrap/app.go
@@ -2,6 +2,8 @@ package bootstrap
 
 import (
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
+	"github.com/gofiber/fiber/v3/middleware/logger"
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	"github.com/Empathify-FICPACT/Empathify-BE/internal/routes"
@@ -11,6 +13,18 @@ func NewApp(db *pgxpool.Pool) *fiber.App {
 	app := fiber.New(fiber.Config{
 		AppName: "Empathify API",
 	})
+
+	app.Use(logger.New())
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"http://localhost:3000",
+		},
+		AllowMethods: []string{"GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+	}))
+
 
 	routes.Route(app, db)
 
