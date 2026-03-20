31cf8876caeeaef51c4aeb65884668420e567cb1
GantangSatria <gantang610@gmail.com>
2026-03-20 14:56:33 +0700

    feat(auth): added auth response dto

---
+++ pkg/dto/response/auth_response.go
@@ -0,0 +1,13 @@
+package response
+
+type AuthResponse struct {
+	AccessToken string   `json:"access_token"`
+	User        UserData `json:"user"`
+}
+
+type UserData struct {
+	ID       string  `json:"id"`
+	Name     *string `json:"name"`
+	Gender   string  `json:"gender"`
+	AvatarID int16   `json:"avatar_id"`
+}
