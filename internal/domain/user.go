7bca1c0b9a79cdabf46425a25b594eb3573caa74
GantangSatria <gantang610@gmail.com>
2026-03-20 14:54:33 +0700

    feat(auth): added auth domain for user

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,1 +1,26 @@
 package domain
+
+import "time"
+
+type User struct {
+	ID             string
+	Name           *string
+	Gender         string
+	AvatarID       int16
+	CurrentStreak  int
+	LongestStreak  int
+	LastActiveDate *time.Time
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+}
+
+type UserAuth struct {
+	ID           string
+	UserID       string
+	Provider     string
+	ProviderUID  *string
+	Email        string
+	PasswordHash *string
+	IsVerified   bool
+	CreatedAt    time.Time
+}
