c977511d7abe064ab29ae456b07bc0a661ecec73
GantangSatria <gantang610@gmail.com>
2026-03-27 17:02:46 +0700

    fix(cors): updated cors rules

--- internal/bootstrap/app.go
+++ internal/bootstrap/app.go
@@ -20,6 +20,7 @@ func NewApp(db *pgxpool.Pool) *fiber.App {
 		AllowOrigins: []string{
 			"http://localhost:3000",
 			"https://empathifyy.vercel.app",
+			"https://empathify.vercel.app",
 		},
 		AllowMethods: []string{"GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders: []string{"Origin", "Content-Type", "Authorization"},
